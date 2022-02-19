bd918cdfbc93f76df951b929325595fc09f50325
Hikaru <hkak03key@gmail.com>
2022-02-19 13:24:35 +0000

    rm resource (will not use in this provider)

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -29,9 +29,6 @@ func New(version string) func() *schema.Provider {
 			DataSourcesMap: map[string]*schema.Resource{
 				"scaffolding_data_source": dataSourceScaffolding(),
 			},
-			ResourcesMap: map[string]*schema.Resource{
-				"scaffolding_resource": resourceScaffolding(),
-			},
 		}
 
 		p.ConfigureContextFunc = configure(version, p)
